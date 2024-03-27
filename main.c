384d4a4a7cc6ec60edd83c156849e75bf2da5e0d
Valeryio <tommagoma23@gmail.com>
2024-03-27 08:09:12 +0000

    Getting the namefile

--- main.c
+++ main.c
@@ -5,12 +5,20 @@ int main(int argc, char* argv[])
 	FILE *myfile = NULL;
 //	char filename = strlen(argv[1]);
 	char *filename = NULL;
+
+/*Verifing if the program have been well used like the prototype 
+ * said : monty <file> 
+ **/
 	if (argc <= 1 || argc > 2)
 	{
 		printf("USAGE: monty file");
 		exit(EXIT_FAILURE);
 	}
-
+/*
+ * Getting the name of the file tohave a better code readibility than just
+ * write with an incomprehensible name of variable! @filename is better than
+ * argv[1]!
+ **/
 	filename = malloc(sizeof(char) * (strlen(argv[1]) + 1));
 	if (!filename)
 	{
@@ -23,7 +31,22 @@ int main(int argc, char* argv[])
 
 	printf("Voici le nom du fichier : %s\n", filename);
 
+/*
+ * Try to open the file
+ */
+	myfile = fopen(filename, "r");
+
+	if (!myfile)
+	{
+		printf("Error, Can't open file %s\n", filename);
+		exit(EXIT_FAILURE);
+	}
 //	myfile = fopen("")
+
+
+
+
+	fclose(myfile);
 
 	return (0);
 }
