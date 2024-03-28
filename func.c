72f3d29233e3d6722249961936f29b11b1b48a80
Dawit-Zeyede <devazeyede10@gmail.com>
2024-03-28 11:35:11 -0700

    pus

--- func.c
+++ func.c
@@ -21,22 +21,44 @@ char *_strdup(char *str)
 	}
 	return (NULL);
 }
+/**
+ * push - this function push an node
+ * to a stack
+ *
+ * @head: the head of the link
+ * @n: the data of the node
+ * Description: This function adds node
+ * to an existing stack.
+ * Return: Nothing
+ */
 
-void pus(stack_t **stack, unsigned int line_number)
+void pus(stack_t **head, unsigned int n)
 {
-	        stack_t *new = malloc(sizeof(stack_t));
-		        if (new == NULL)
-				        {
-						                fprintf(stderr, "Memory allocation failed\n");
-								                exit(EXIT_FAILURE);
-										        }
-			        new->n = line_number;
-				        new->prev = NULL;
-					        new->next = *stack;
-						        if ((*stack) != NULL)
-								                (*stack)->prev = new;
-							        (*stack) = new;
+	stack_t* newnode = NULL;
+
+	newnode = malloc(sizeof(stack_t));
+	if (!newnode)
+	{
+		printf("Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	newnode->n = n;
+	/*In this case where the stack is empty!*/
+	if (!(*head))
+	{
+		newnode->next = NULL;
+		newnode->prev = NULL;
+		(*head) = newnode;
+		return;
+	}
+
+/*What is the stack is not empty?*/
+	newnode->prev = NULL;
+	newnode->next = (*head);
+	(*head) = newnode;
 }
+
 void pal(stack_t **stack, unsigned int line_number)
 {
 	        stack_t *current = *stack;
